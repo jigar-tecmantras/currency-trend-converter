18255df7b775ad85c98e8e77a5b9fa49acce4ac5
jigar-tecmantras <73215263+jigar-tecmantras@users.noreply.github.com>
2026-07-08 13:23:05 +0530

    Fix Chart.js auto import

--- frontend/src/components/TrendChart.js
+++ frontend/src/components/TrendChart.js
@@ -1,17 +1,6 @@
 import React from "react";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
-  Filler
-} from "chart.js";
+import "chart.js/auto";
 import { Line } from "react-chartjs-2";
-
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend, Filler);
 
 const TrendChart = ({ history }) => {
   if (!history || history.length === 0) {
